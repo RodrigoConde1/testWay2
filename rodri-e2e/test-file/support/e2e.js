aa3c0d565ad83e3eb1053b10eb250069b7eb86e6
RodrigoConde1 <85459650+RodrigoConde1@users.noreply.github.com>
2024-05-11 19:28:03 +0200

    test

--- rodri-e2e/test-file/support/e2e.js
+++ rodri-e2e/test-file/support/e2e.js
@@ -15,7 +15,6 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
-import 'cypress-mochawesome-reporter/register';
 const registerCypressGrep = require('@cypress/grep')
 registerCypressGrep()
 
