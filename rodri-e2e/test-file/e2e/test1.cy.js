12624f466cf4a41bf643de09c1bff1b50e2da9cb
RodrigoConde1 <85459650+RodrigoConde1@users.noreply.github.com>
2024-05-17 19:20:19 +0200

    a1

--- rodri-e2e/test-file/e2e/test1.cy.js
+++ rodri-e2e/test-file/e2e/test1.cy.js
@@ -3,6 +3,16 @@
 /// <reference types="cypress" />
 
 it('runs test A1',  { tags: ['config', 'some-other-tag'] }, () => {
+  const envVariables = {
+    split: Cypress.env('split'),
+    splitIndex: Cypress.env('splitIndex'),
+    splitFile: Cypress.env('splitFile'),
+    skipSpec: Cypress.env('skipSpec'),
+    debug: Cypress.env('debug'),
+    grepTags: Cypress.env('grepTags')
+  };
+
+  cy.writeFile('cypress/envVariables.json', envVariables);
   const splitIndex = Cypress.env('SPLIT_INDEX');
 
   cy.log(splitIndex);
