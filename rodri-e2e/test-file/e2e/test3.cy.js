c6612a1dc0540a9a3612c8ae906f6f6f55072a12
RodrigoConde1 <85459650+RodrigoConde1@users.noreply.github.com>
2024-06-07 16:16:04 +0200

    2

--- rodri-e2e/test-file/e2e/test3.cy.js
+++ rodri-e2e/test-file/e2e/test3.cy.js
@@ -9,7 +9,7 @@ let user
 it('runs test C1', { tags: ['config', 'some-other-tag'] }, () => {
   user = utilsAction.selectUser()
   cy.log(user)
-  expect(user).to.be.eq('rodrigo+1')
+  expect(user).to.be.eq('rodrigo+2')
 
   cy.wait(10_000)
 })
